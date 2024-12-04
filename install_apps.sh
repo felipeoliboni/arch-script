cbbe58c8b35a46c354e532c6d45a995894a1653c
felipeoliboni <foliboni@gmail.com>
2024-12-04 10:25:15 -0300

    added packages

--- install_apps.sh
+++ install_apps.sh
@@ -10,10 +10,22 @@ packages=(
   visual-studio-code-bin
   github-cli
   btop
+  filezilla
+)
+
+packages_yay=(
+    google-chrome
+    anydesk-bin
+    mysql-workbench
+    chromium
 )
 
 for PKG1 in "${packages[@]}"; do
   sudo pacman -S --noconfirm "$PKG1"
+done
+
+for PKG1 in "${packages_yay[@]}"; do
+  yay -S --noconfirm "$PKG1"
 done
 
 clear
